7998028c5f8f7e8c8c944e9ecad6249974936639
hannahEDULEO <hannahflunkert95@gmail.com>
2026-06-08 22:13:22 +0200

    worker

--- worker.js
+++ worker.js
@@ -1,5 +1,19 @@
 export default {
   async fetch(request, env) {
-    return env.ASSETS.fetch(request);
+    const url = new URL(request.url);
+    let path = url.pathname;
+
+    // Direkter Versuch
+    let response = await env.ASSETS.fetch(request);
+
+    // Bei 404: index.html im Verzeichnis versuchen
+    if (response.status === 404) {
+      if (!path.endsWith('/')) path += '/';
+      const indexUrl = new URL(request.url);
+      indexUrl.pathname = path + 'index.html';
+      response = await env.ASSETS.fetch(new Request(indexUrl.toString(), request));
+    }
+
+    return response;
   }
 };
